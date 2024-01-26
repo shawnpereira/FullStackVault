f879e4fc31d51d0fd30f5e392fe4d4286827ed00
shawn <shawnpereiraswp@gmail.com>
2024-01-26 14:00:00 +0100

    css circle animation

--- ReactHooks/src/App.jsx
+++ ReactHooks/src/App.jsx
@@ -1,61 +1,7 @@
-import { useState } from "react";
-import { useEffect } from "react";
+import React from "react";
 
-function App() {
-  const [number, setNumber] = useState(1);
-  return (
-    <div>
-      <button
-        onClick={function () {
-          setNumber(1);
-        }}
-      >
-        1
-      </button>
-      <button
-        onClick={function () {
-          setNumber(2);
-        }}
-      >
-        2
-      </button>
-      <button
-        onClick={function () {
-          setNumber(3);
-        }}
-      >
-        3
-      </button>
-      <button
-        onClick={function () {
-          setNumber(4);
-        }}
-      >
-        4
-      </button>
-      <Todo id={number} />
-    </div>
-  );
-}
-
-function Todo({ id }) {
-  const [todo, setTodo] = useState({});
-
-  useEffect(() => {
-    fetch("https://sum-server.100xdevs.com/todo?id=" + id).then(async function (
-      res
-    ) {
-      const json = await res.json();
-      setTodo(json.todo);
-    });
-  }, [id]);
-
-  return (
-    <div>
-      <h1>{todo.title}</h1>
-      <h4>{todo.description}</h4>
-    </div>
-  );
-}
+const App = () => {
+  return <div>App</div>;
+};
 
 export default App;
