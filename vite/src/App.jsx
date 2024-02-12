29b0c03e235ebb3e74709db24eabdfa2df242025
shawn <shawnpereiraswp@gmail.com>
2024-02-12 14:00:00 +0100

    starter template react

--- vite/src/App.jsx
+++ vite/src/App.jsx
@@ -1,17 +1,5 @@
-import { useState } from "react";
-
-import "./App.css";
-
-function App() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <div>
-      <button onClick={() => setCount((count) => count + 1)}>
-        count is {count}
-      </button>
-    </div>
-  );
-}
+const App = () => {
+  return <div>MERN</div>;
+};
 
 export default App;
