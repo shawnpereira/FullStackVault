3944e2500ac9f32178f961ac454c274106466f63
shawn <shawnpereiraswp@gmail.com>
2024-02-13 22:32:53 +0530

    simple counter

--- vite/src/App.jsx
+++ vite/src/App.jsx
@@ -1,5 +1,16 @@
+import { useState } from "react";
+
 const App = () => {
-  return <div>MERN</div>;
+  const [count, setCount] = useState(0);
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  return (
+    <div>
+      <button onClick={increment}>{count}</button>
+    </div>
+  );
 };
 
 export default App;
