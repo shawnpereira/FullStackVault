78cf45735254bf94a8cd8ee2ce93a811e72c9033
shawn <shawnpereiraswp@gmail.com>
2024-03-01 14:00:00 +0100

    file fix

--- ReactHooks/src/App.jsx
+++ ReactHooks/src/App.jsx
@@ -1,24 +1,7 @@
-import { useEffect, useRef } from "react";
-//useRef is a Hook in React that allows you to create a mutable reference to a DOM element or any other value that persists across renders without causing a re-render when its value changes.
+import React from "react";
 
-//useRef gives you a way to keep track of values that persist between renders without causing the component to re-render. It's useful for accessing or storing references to DOM elements, managing focus, or storing any value that you want to persist between renders without triggering a re-render.
-function App() {
-  const containerRef = useRef();
-
-  useEffect(() => {
-    setTimeout(() => {
-      containerRef.current.innerHTML = "10";
-    }, 5000);
-  }, []);
-
-  const incomeTax = 20000;
-
-  return (
-    <div>
-      hi there, your income tax returns are{" "}
-      <div ref={containerRef}>{incomeTax}</div>
-    </div>
-  );
-}
+const App = () => {
+  return <div>App</div>;
+};
 
 export default App;
