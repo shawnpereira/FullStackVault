73e2172d509fea75d4484f41fa39ab9c30e59ffb
shawn <shawnpereiraswp@gmail.com>
2024-04-30 14:00:00 +0100

    passGen

--- ReactConcepts/src/App.jsx
+++ ReactConcepts/src/App.jsx
@@ -1,11 +1,26 @@
-import UseRefAssignOne from "./hooks/useRef/UseRefAssignOne";
+import React, { useCallback, useState } from "react";
 
 const App = () => {
-  return (
-    <div>
-      <UseRefAssignOne />
-    </div>
-  );
+  const [length, setlength] = useState(8);
+  const [number, setNumber] = useState(false);
+  const [char, setChar] = useState(false);
+
+  const [password, setPassword] = useState("");
+
+  const passwordGenerator = useCallback(() => {
+    let pass = "";
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    if (number) str += "0123456789";
+    if (char) str += "!@#$%^&*-_+=[]{}~`";
+
+    for (let i = 1; i <= length; i++) {
+      let char = Math.floor(Math.random() * str.length + 1);
+      pass += str.charAt(char);
+    }
+
+    setPassword(pass);
+  }, [length, number, char, setPassword]);
+  return <div>App</div>;
 };
 
 export default App;
---
+++ ReactConcepts/src/hooks/Ref,Callback,UseEffect/PasswordGen.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const PasswordGen = () => {
+  return <div>PasswordGen</div>;
+};
+
+export default PasswordGen;
